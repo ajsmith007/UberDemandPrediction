33aa5153fa12ff2d3ab1ca725b78869518e96dac
ajsmith <ajsmith007@gmail.com>
2013-09-26 09:06:42 -0700

    Version 2013.09.26 - Completed Backend 	- Additional css styling for headers 	- Added further instructions for venv 	- Added content to pages 	- Added gitignore for .Rhistory and .RData extensions 	- Extended API bootsrap container to col-md-10 to fit full API href

--- demandPredict.py
+++ demandPredict.py
@@ -1,29 +1,40 @@
 #!flask/bin/python
 
 '''
+########################################################################################
 demandPredict.py
 
 Coding Challenge for Uber
 Demand Prediction: Washington D.C. 
 
-Created on Sep 20, 2013
-Completed on Sep 24, 2013
-@author: ajsmith007@gmail.com
-'''
+__author__ = "Drew Smith" 
+__email__ = "ajsmith007@gmail.com"
+__version__ = "1.0.0"
+__status__ = "Demonstration"
+__copyright__ = Copyright 2013, Drew Smith"
 
+Assigned on: Sept 18, 2013
+Created on: Sep 20, 2013
+Completed on: Sep 26, 2013
+
+########################################################################################
+'''
 from flask import Flask, jsonify, render_template, send_from_directory, request, make_response
 import os
 import jinja2
 import datetime
 import json
 import csv
+import dateutil.parser
 
-VERSION = "2013.09.24"
+########################################################################################
+VERSION = "2013.09.26"
 jinja_environment = jinja2.Environment(autoescape = True, # cgi escape set to autoescape
                                        loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
 app = Flask(__name__)
 
+########################################################################################
 # Original and Derived Data
 data = []
 with open('static/data/uber_demand_prediction_challenge.json') as f:
@@ -46,137 +57,75 @@ for row in csvReader:
     slope.append(row)
 # access elements by index: print slope[0][1] = Sunday 0100
 
-
-# Temporary Data
-prediction = [
-    {
-        'year': 2012,
-        'month': 4,
-        'day': 30,
-        'hour': 20,
-        'doy': 121,
-        'dow': u'Monday',
-        'n_dow': u'1-Monday',
-        'date': u'04-30-2012',
-        'datetime_et': u'04-30-2012T20:00:00 EST',
-        'datetime_utc': u'05-01-2012T00:00:00 UTC',
-        'datetime': u'05-01-2012T00:00:00',
-        'prediction': 33.02
-    },
-    {
-        'year': 2012,
-        'month': 4,
-        'day': 30,
-        'hour': 21,
-        'doy': 121,
-        'dow': u'Monday',
-        'n_dow': u'1-Monday',
-        'date': u'04-30-2012',
-        'datetime_et': u'04-30-2012T21:00:00 EST',
-        'datetime_utc': u'05-01-2012T01:00:00 UTC',
-        'datetime': u'05-01-2012T01:00:00',
-        'prediction': 42.96
-    },
-    {
-        'year': 2012,
-        'month': 4,
-        'day': 30,
-        'hour': 22,
-        'doy': 121,
-        'dow': u'Monday',
-        'n_dow': u'1-Monday',
-        'date': u'04-30-2012',
-        'datetime_et': u'04-30-2012T22:00:00 EST',
-        'datetime_utc': u'05-01-2012T02:00:00 UTC',
-        'datetime': u'05-01-2012T02:00:00',
-        'prediction': 12.03
-    },
-    {
-        'year': 2012,
-        'month': 4,
-        'day': 30,
-        'hour': 23,
-        'doy': 121,
-        'dow': u'Monday',
-        'n_dow': u'1-Monday',
-        'date': u'04-30-2012',
-        'datetime_et': u'04-30-2012T23:00:00 EST',
-        'datetime_utc': u'05-01-2012T03:00:00 UTC',
-        'datetime': u'05-01-2012T03:00:00',
-        'prediction': 10.6
-    },
-    {
-        'year': 2012,
-        'month': 5,
-        'day': 1,
-        'hour': 0,
-        'doy': 122,
-        'dow': u'Tuesday',
-        'n_dow': u'2-Tuesday',
-        'date': u'05-01-2012',
-        'datetime_et': u'05-01-2012T00:00:00 EST',
-        'datetime_utc': u'05-01-2012T04:00:00 UTC',
-        'datetime': u'05-01-2012T04:00:00',
-        'prediction': 6.05
-    },
-]
-
+########################################################################################
 # Prep Data and Demand Prediction Model
-def predictFutureDemand(self, dt):
-    # Model of future demand from input datetime
-    prediction = 99         # prediction placeholder
-    
-    # Prediction Linear Model
-    #
-    #
-    #
-    #
-    predictionLinear = 99
-
-    # Prediction Regression Tree Model
-    #
-    #
-    #
-    #
-    predictionTree = 99
-    
+def predictFutureDemand(inputStr):
+    # Model of future demand from input utc datetime
+    verbose = True
+    if verbose == True: print inputStr
+    # Convert input ISO format datetime "2012-05-01T00:00:00" to python datetime object
+    datetime_utc = dateutil.parser.parse(inputStr)
+    if verbose == True: print datetime_utc
+    # Compute which Day of Epoch (doe)
+    doeObj = datetime_utc - datetime.datetime.utcfromtimestamp(0)
+    doe = int(doeObj.days)
+    if verbose == True: print doe
+    # Convert input UTC time to local date time 
+    local_dt = datetime_utc
+    if verbose == True: print local_dt
+    # Extract which Day of the Week as numeric (1=Mon, .., 7=Sun etc)
+    wkday = local_dt.weekday()
+    if (wkday == 7):            # coeff table has 0=Sun, 1=Mon, ..., etc
+        dow = 0
+    else:
+        dow = int(wkday)
+    #dow = 0    # debugging
+    if verbose == True: print dow
+    # Extract which Hour of the Day (0-23)
+    hr = int(local_dt.strftime("%H"))
+    #hr = 3     # debugging
+    if verbose == True: print hr
+    # Look up Linear Model Coefficents
+    b = intercept[dow][hr]
+    if verbose == True: print b
+    m = slope[dow][hr]
+    if verbose == True: print m
+    # Compute Demand Prediction from the RLM for the given UTC date and time in pp.pp 
+    prediction = float(m)*float(doe) + float(b)
+    #prediction = -99.99     # placeholder for debug
+    if verbose == True: print prediction
     return prediction  
       
-def generatePredictionVars(self, futuredt):
-    # Convert input ISO format datetime "2012-05-01T00:00:00" to python datetime (dt) object
-    import dateutil.parser
-    dt = dateutil.parser.parse(futuredt)
-        
-    # Computed derived vars for predict model
-    doe = dt - datetime.datetime.utcfromtimestamp(0)
-#         epoch = datetime.datetime.utcfromtimestamp(0)
-#         #print epoch
-#         #1970-01-01 00:00:00
-#         #today = datetime.datetime.today()
-#         d = today - epoch
-#         #print d
-#         #13196 days, 9:50:44.266200
-#         #print d.days # timedelta object
-#         #13196
-    
-    doy = dt.timetuple().tm_yday
-    dow = dt.strftime("%A")
-    n_dow = dt.isoweekday()
-    
-    prediction = predictFutureDemand(dt)
-    
-    # Compile results into a dict
-    results = {'doe': doe.day,
-               'doy': doy,
-               'dow': dow,
-               'n_dow': n_dow,
-               'prediction': prediction
-    }
-    
-    # Return json object of prediction with derived vars
-    return jsonify(results)
+# def generatePrediction(self, futuredt):
+#     # Convert input ISO format datetime "2012-05-01T00:00:00" to python datetime (dt) object
+#     import dateutil.parser
+#     utc_dt = dateutil.parser.parse(futuredt)
+#         
+#     # Computed derived vars for predict model
+#     doe = utc_dt - datetime.datetime.utcfromtimestamp(0) # day of epoch - pass full object need to call as.numeric
+# #         epoch = datetime.datetime.utcfromtimestamp(0)
+# #         #print epoch
+# #         #1970-01-01 00:00:00
+# #         #today = datetime.datetime.today()
+# #         d = today - epoch
+# #         #print d
+# #         #13196 days, 9:50:44.266200
+# #         #print d.days # timedelta object
+# #         #13196
+#     local_dt = utc_dt
+#     #doy = local_dt.timetuple().tm_yday
+#     dow = local_dt.strftime("%A")
+#     n_dow = local_dt.isoweekday()
+#     hour = local_dt.strftime("%H")
+#     prediction = predictFutureDemand(local_dt)
+#     
+#     # Tuple of prediction in a dict
+#     #prediction = {'prediction': prediction}
+#     
+#     # Return prediction float
+#     return prediction
 
-
+########################################################################################
 # Error Responses
 @app.errorhandler(400)
 def not_found400(error):
@@ -186,7 +135,7 @@ def not_found400(error):
 def not_found404(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
-
+########################################################################################
 # Routes
 @app.route('/')
 @app.route('/index/')
@@ -259,23 +208,29 @@ def getModel():
 
 @app.route('/api/v1/prediction', methods = ['GET'])
 def getPrediction():
-    if request.method == 'GET':
-        return jsonify( { 'prediction': prediction } )
-    return make_response(jsonify( { 'error': 'Not found' } ), 404) #abort(404) #  GET request not included
+    queryStr = request.args.get('q')
+    if len(queryStr) == 0:
+        return make_response(jsonify( { 'error': 'Not found' } ), 404) #abort(404) 
+    # Might need to check that the format of queryStr is valid ISO date time
+    if len(queryStr) != 19:
+        return make_response(jsonify( { 'error': 'Bad request' } ), 400) # malformed GET request
     
-@app.route('/api/v1/prediction/<datetime>', methods = ['GET'])
-def getPredictionDateTime(datetime):
-    predict = filter(lambda t: t['datetime'] == datetime, prediction)
-    if len(predict) == 0:
-        return make_response(jsonify( { 'error': 'Not found' } ), 404) #abort(404) #  datetime not found
-    return jsonify( { 'prediction': predict[0] } )
+    prediction = predictFutureDemand(queryStr)
+    return jsonify( {"UberDemandPrediction": [{'datetime': queryStr }, { 'prediction': prediction }] })
+    
+# @app.route('/api/v1/prediction/<datetime>', methods = ['GET'])
+# def getPredictionDateTime():
+#     #predict = filter(lambda t: t['datetime'] == datetime, prediction)
+#     if len(predict) == 0:
+#         return make_response(jsonify( { 'error': 'Not found' } ), 404) #abort(404) #  datetime not found
+#     return jsonify( { 'prediction': predict[0] } )
 
 # favicon
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'img/favicon.ico')
 
-
+########################################################################################
 # Main
 if __name__ == '__main__':
     app.run(debug = True)
